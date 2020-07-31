c1328575bcc5341ed26403d0256fff4d15bfe0c5
Kristers <kristers.nikitins@inbox.lv>
2020-07-31 09:53:31 +0300

    Creating POJO's (Issues #2)

---
+++ src/main/java/xyz/petclinic/springpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package xyz.petclinic.springpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/xyz/petclinic/springpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package xyz.petclinic.springpetclinic.model;
+
+public class Vet extends Person {
+}
