03cd15eb9e79c32036ec350dc38bfce1b41d5883
Kristers <kristers.nikitins@inbox.lv>
2020-08-04 14:17:04 +0300

    Created speciality service

---
+++ pet-clinic-data/src/main/java/xyz/petclinic/springpetclinic/services/SpecialityService.java
@@ -0,0 +1,8 @@
+package xyz.petclinic.springpetclinic.services;
+
+import xyz.petclinic.springpetclinic.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long> {
+
+
+}
