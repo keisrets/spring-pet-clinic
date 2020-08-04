c6c5169af250fb64b7f4962635460bda2f544590
Kristers <kristers.nikitins@inbox.lv>
2020-08-04 09:50:05 +0300

    Created Speciality entity and associated it with Vet

---
+++ pet-clinic-data/src/main/java/xyz/petclinic/springpetclinic/model/Speciality.java
@@ -0,0 +1,14 @@
+package xyz.petclinic.springpetclinic.model;
+
+public class Speciality extends BaseEntity {
+
+    private String speciality;
+
+    public String getSpeciality() {
+        return speciality;
+    }
+
+    public void setSpeciality(String speciality) {
+        this.speciality = speciality;
+    }
+}
