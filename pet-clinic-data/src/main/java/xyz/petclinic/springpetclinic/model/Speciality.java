3d45380df9cc8a1f4d289a4548b011cd0f540088
Kristers <kristers.nikitins@inbox.lv>
2020-08-05 13:11:26 +0300

    Completed Vet entity JPA mapping

--- pet-clinic-data/src/main/java/xyz/petclinic/springpetclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/xyz/petclinic/springpetclinic/model/Speciality.java
@@ -1,7 +1,14 @@
 package xyz.petclinic.springpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "specialities")
 public class Speciality extends BaseEntity {
 
+    @Column(name = "speciality")
     private String speciality;
 
     public String getSpeciality() {
